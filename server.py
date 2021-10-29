a2203da42d45e510b45d0737cc53ba7d8078f8d0
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-29 16:52:54 -0700

    add session and redirect

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import datetime
 from flask import Flask, render_template, session
 from werkzeug.datastructures import RequestCacheControl
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 app.secret_key = "TiYSKDNRitA!"                                                     # This is Your Secret Key Do Not Reveal it to Anyone!
@@ -8,10 +9,17 @@ app.secret_key = "TiYSKDNRitA!"                                                 
 @app.route('/')                                                                     # Main Page
 def index():
     if 'num_visits' in session:
+        session['num_visits'] += 1
         print("Session key num_visits:", session['num_visits'] )
     else:
         session['num_visits'] = 1
+         print("Session key num_visits:", session['num_visits'] )
     return render_template("index.html")
+
+@app.route('/clear')
+def clear():
+    session.clear();
+    return redirect("/")
 
 if __name__=="__main__":   
     app.run(debug=True)    
